a2422b4892b075d1238ea3c7293ca7a8547950a3
Ipro100 <40054626+Ipro100@users.noreply.github.com>
2018-11-24 21:37:25 +0300

    1

--- js/common.js
+++ js/common.js
@@ -14,13 +14,17 @@ $(document).ready(function() {
    $(".header_content h1").animated("fadeInDown");
    $(".line_caption p, .section_header").animated("fadeInUp");
    $(".line_caption").animated("fadeInUp");
+   $(".portfolio_item").animated("zoomIn");
    $(".mnu_line").animated("zoomIn");
    
    $(".anim_zoom ").animated("zoomIn","zoomOut");
    $(".anim_rotate_left ").animated("rotateInUpRight");
    $(".anim_rotate_right ").animated("rotateInUpLeft");
+  
+   $(".animLeft ").animated("fadeInLeft");
+   $(".animRight ").animated("fadeInRight");
 
-
+   $(".animFadeIn").animated("fadeIn");
 
 	//SVG Fallback
 	if(!Modernizr.svg) {
@@ -47,6 +51,38 @@ $(document).ready(function() {
 	
 	$(".mnu_line ul li a").mPageScroll2id();
 
+
+
+    $('#form').submit(function() { // проверка на пустоту заполненных полей. Атрибут html5 — required не подходит (не поддерживается Safari)
+    if (document.form.name.value == '' || document.form.phone.value == '' ) {
+      valid = false;
+      return valid;
+    }
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $('.js-overlay-thank-you').fadeIn();
+      $(this).find('input').val('');
+      $('#form').trigger('reset');
+    });
+    return false;
+  });
+
+
+});
+
+// Закрыть попап «спасибо»
+$('.js-close-thank-you').click(function() { // по клику на крестик
+  $('.js-overlay-thank-you').fadeOut();
+});
+
+$(document).mouseup(function (e) { // по клику вне попапа
+    var popup = $('.popup');
+    if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+        $('.js-overlay-thank-you').fadeOut();
+    }
 });
 
 $(window).on('load', function () {
