d648362824ed6a518da42663d67fe99ed61f8cee
Ipro100 <40054626+Ipro100@users.noreply.github.com>
2018-11-28 13:52:51 +0300

    2

--- royal/js/common.js
+++ royal/js/common.js
@@ -4,8 +4,8 @@ $('.center').slick({
   centerMode: true,
   centerPadding: '0',
   slidesToShow: 3,
-  // autoplay: true,
-  // autoplaySpeed: 5000,
+  autoplay: true,
+  autoplaySpeed: 5000,
   responsive: [
  
     {
@@ -21,8 +21,44 @@ $('.center').slick({
 });
 
 	
-	// $(".popup_content").magnificPopup({
-	// 	type:"inline",
-	// 	midClick: true
-	// });
+	$(".popup_content").magnificPopup({
+		type:"inline",
+		midClick: true
+	});
+
+
+	$("#form-button").click(function() {
+		var chek = checkParams();
+		if(chek){
+		$.magnificPopup.close();
+		
+	}
+	});
+
+$('#contact').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+
 });
+
+function checkParams() {
+    var name = $('#name').val();
+    var phone = $('#phone').val();  
+    if(name.length != 0 && phone.length != 0 ) {
+        return true;
+      }
+       else {
+       return false;
+    }
+    
+}
