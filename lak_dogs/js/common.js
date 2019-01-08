1c6b1463f3e9fa4071c3955f37228d8a216ebc90
Ipro100 <40054626+Ipro100@users.noreply.github.com>
2019-01-09 02:47:52 +0300

    dog

---
+++ lak_dogs/js/common.js
@@ -0,0 +1,47 @@
+$(function() {
+
+
+
+
+	$('.s_slider').slick({
+	dots: true,
+  slidesToShow: 4,
+  loop: true,
+  slidesToScroll: 1,
+  centerPadding: '60',
+  // autoplay: true,
+  autoplaySpeed: 5000,
+  responsive: [
+  
+    
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+
+    {
+      breakpoint: 460,
+      settings: {
+        // arrows: false,
+        centerPadding: '60',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 360,
+      settings: {
+        // arrows: false,
+        centerPadding: '60',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+
+
+
+});
