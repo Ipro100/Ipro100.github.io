522ec996075d11853226faae77ae1a0ba3cf3a91
Ipro100 <40054626+Ipro100@users.noreply.github.com>
2019-03-17 19:09:48 +0300

    auto

--- autok/js/common.js
+++ autok/js/common.js
@@ -1,49 +1,51 @@
 $(function() {
 
  var images  = $('.rotate');
+ var images1  = $('.rotate1');
  var i = 0;
+ var j = 0;
 
 	$(window).scroll(function() {
-
-        window.requestAnimationFrame(function() {
-        	 isScrolling();
-        	
-        });
+		
+		window.requestAnimationFrame(function() {
+        	isScrolling(); 
+        	    	
+    });
 
 
 });
 
 	function isScrolling(){
 		 if ($(this).scrollTop() > 0) {
-        console.log('scroll');
+        
 
         images[i].classList.remove('active');
-        	i++;
+        images1[j].classList.remove('active');
+       
+        	i++; 
+        	j++;
 			 	if(i > images.length - 1){
 			 		i = 0;
+			 	}			 	
+			 	if(j > images1.length - 1){
+			 		j = 0;
 			 	}
 			 	images[i].classList.add('active'); 
+			 	images1[j].classList.add('active'); 
 
     }
-	}
+	}	
 
-
-// 	var isScrolling = false;
- 
-// window.addEventListener("scroll", throttleScroll, false);
- 
-// function throttleScroll(e) {
-//     if (isScrolling == false ) {
-//         window.requestAnimationFrame(function() {
-//           dealWithScrolling(e);
-//           isScrolling = false;
-//         });
-//     }
-//     isScrolling = true;
-// }   
- 
-// function dealWithScrolling(e) {
-//     console.log('scroll1'); 
-// }
-
+	// var controller = new ScrollMagic.Controller()
+	// var scene = new ScrollMagic.Scene({
+	// 		triggerElement: '.logo',
+	// 		duration: 1000
+	// })
+	// .setClassToggle('.s1_advantage .item', 'fade-in')
+	// .addIndicators({
+	// 	name: 'fade scene',
+	// 	colorTrigger: 'black',
+	// 	colorStart: 'red'
+	// })
+	// .addTo(controller);
 });
