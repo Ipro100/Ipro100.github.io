5484462ef15b654053ea4d3d7f2f4ce5ec3cd636
Ipro100 <igorpro1990@gmail.com>
2020-05-27 18:20:32 +0300

    mrl

--- marvel/js/_custom.js
+++ marvel/js/_custom.js
@@ -1,112 +1,88 @@
 $(function(){
 
 
-// $('.title__img').animated('fadeInRight');
-// $('.product-title').animated('fadeIn');
-// $('.product__slang-img').animated('fadeInRight');
-// $('.s3__item').animated('fadeIn');
-// $('.product-label').animated('zoomIn');
-// $('h1 span:last-child	span').animated('zoomIn');
-
-
-// const width = window.screen.width;
-
-// if(width < 1900 && width > 992) {
-// const	res = (width - 1900)/2;
-// $('.mask').css('background-position', res + 'px 50%');
-
-// }
-
-// // 231
-// const titleWidth = () => {
-
-// const offset1 =  $('.product__slang').offset().left + 231;
-// const offset2 =  $('.product__slang').offset().left + 281;
-
-// $('.offset1').css('width',offset1);
-// $('.offset2').css('width',offset2);
-
-// }
-
-// titleWidth();
-
-// $( window ).resize(function() {
-//   titleWidth();
-// });
 
 
 
 
+	$.fn.elem = function(inEffect, outEffect) {
+		$(this).each(function() {
+			var ths = $(this);
+			var inter;
+      if(!(ths.hasClass('animated'))) {
 
-
-
-
-
-
-// 	$.fn.elem = function(inEffect, outEffect) {
-// 		$(this).each(function() {
-// 			var ths = $(this);
-//       if(!(ths.hasClass('animated'))) {
-
-// 			ths.css("opacity","1").addClass("lazyload").waypoint(function(dir) {
+			ths.css("opacity","1").addClass("lazyload").waypoint(function(dir) {
             
-// 						if (dir === "down") {
-// 								ths.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
-//                 ths.addClass('animated');  
-//                 setTimeout(move,0,ths.find('.slide'));
-    
-// 						} else {
-// 								ths.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
-                
+						if (dir === "down") {
+								ths.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+                ths.addClass('animated'); 
+                if(ths.hasClass('anim2')){
+              	inter =	setInterval(move, 1000, ths.find('img'));
+                } 
+                else if(ths.hasClass('anim1')) { 
+               inter = setInterval(move, 2500, ths.find('img'));
+   							 }
+						} else {
+								ths.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+                clearInterval(inter);
                
-// 						};
-// 			}, {
-// 				offset: "50%"
-// 			});
-// 		ths.waypoint(function(dir) {
-// 					if (dir === "down") {
-// 								ths.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+						};
+			}, {
+				offset: "80%"
+			});
+		// ths.waypoint(function(dir) {
+		// 			if (dir === "down") {
+		// 						ths.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
                 
                 
-// 						} else {
-// 								ths.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+		// 				} else {
+		// 						ths.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
                
                 
-// 						};
-// 			}, {
-// 				offset: -$(window).height()
-// 			});	
-// 	}		
-// 		});
+		// 				};
+		// 	}, {
+		// 		offset: -$(window).height()
+		// 	});	
+	}		
+		});
 
-// 	};
+	};
 
-// function move(arr) {
 
-// let n = 0;
-// let timer = setInterval(function(){
 
-//   $(arr).removeClass('active');
+function move(arr) {
+let n = 0;
+let timer = setInterval(function(){
 
-// n++;
-// console.log(n)
-//   if(n > arr.length - 2){
-//       clearInterval(timer);
-//       console.log("stop");
-//       n = 0;
-//       $(arr[arr.length - 1]).addClass('active');
-//       return;
-//     }  
+  $(arr).removeClass('active');
+
+  if(n > arr.length - 2){
+      clearInterval(timer);
+      n = 0;
+      $(arr[arr.length - 1]).addClass('active');
+      return;
+    }  
  
-//   $(arr[n]).addClass('active');
-// },300);
+  $(arr[n]).addClass('active');
+  n++;
+},100);
 
 
 
-// }
+}
 
 
-//  $('.images').elem('play' ,'stop');
+	 $('.hero-anim').elem('play' ,'stop');
+
+
+
+
+
+
+
+
+
+
 
 
 $('.3items-slide').slick({
@@ -180,5 +156,9 @@ $('.items2-slide').slick({
 });
 
 $('.slick-arrow').html('<span></span>');
+
+$( window ).resize(function() {
+ $('.slick-arrow').html('<span></span>');
+});
 
 });
